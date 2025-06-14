50431305be1c11f2470fe788ba1a668c8b5f3800
chandrashekharbhaupande <chpande55@gmail.com>
2025-06-14 19:55:12 +0530

    code

--- src/main/java/com/jarvis/Department_service/controller/DepartmentController.java
+++ src/main/java/com/jarvis/Department_service/controller/DepartmentController.java
@@ -1,9 +1,29 @@
 package com.jarvis.Department_service.controller;
 
+import com.jarvis.Department_service.model.Department;
+import com.jarvis.Department_service.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @PostMapping
+    public Department add(@RequestBody Department data){
+
+        return departmentRepository.addDepartment(data);
+
+    }
+
+
 }
